1ad3253753d7d34ae11069cdc971f7897fa20db8
kristin.muench <kristin.muench@eligo-bioscience.com>
2024-03-13 23:04:49 +0100

    Initial move of development notebooks

---
+++ notebooks/model_creation/utils/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Run-specific configuration variables to be used throughout notebooks
+
+path_outdir_base = "../../output/20240221_import"
+
+path_counts = "../../../data/raw/reads"
+path_meta = "../../../data/raw/metadata.csv"
+path_ucsc_sexgenes = "../../../data/processed/ucsc/ucsc_sexgene_info.csv" # from 00_prep_get_gene_list
+path_dir_msigdb = "../../../data/raw/msigdb" # downloaded from the msigdb website
